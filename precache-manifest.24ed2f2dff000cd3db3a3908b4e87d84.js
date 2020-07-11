6bfbe1eb2f4734417ad0d02d36b032e873e45dea
Sayantan Ghosh <gsayantan01@gmail.com>
2020-07-12 02:15:16 +0530

    Updates

---
+++ precache-manifest.24ed2f2dff000cd3db3a3908b4e87d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aee610116ef3a8fc5285f81bb4f5c0c",
+    "url": "/rejection/index.html"
+  },
+  {
+    "revision": "a41681e5cc6714f9e1b9",
+    "url": "/rejection/static/css/main.a752c0a2.chunk.css"
+  },
+  {
+    "revision": "fc97d5933851bcf42b99",
+    "url": "/rejection/static/js/2.f8852537.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rejection/static/js/2.f8852537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41681e5cc6714f9e1b9",
+    "url": "/rejection/static/js/main.e1f2f4e2.chunk.js"
+  },
+  {
+    "revision": "cb15d82fe1c95927a1b9",
+    "url": "/rejection/static/js/runtime-main.ad4516c8.js"
+  }
+]);
